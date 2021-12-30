5bd7388799801f239892e56016d53348feee953f
Hai Vu Ngoc (BTS-DL) <hai.vungoc@vib.com.vn>
2021-12-30 16:22:49 +0700

    basic animation

--- src/app/components/win-wheel/win-wheel.component.ts
+++ src/app/components/win-wheel/win-wheel.component.ts
@@ -1,5 +1,4 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
-
 declare let Winwheel: any;
 
 @Component({
@@ -11,6 +10,9 @@ export class WinWheelComponent implements OnInit, AfterViewInit {
   constructor() {}
 
   theWheel: any;
+  wheelPower = 0;
+  wheelSpinning = false;
+  winningSegment: string = '';
 
   ngAfterViewInit(): void {
     this.theWheel = new Winwheel({
@@ -88,8 +90,33 @@ export class WinWheelComponent implements OnInit, AfterViewInit {
             '<p style="margin-bottom: 0px;">Xin chúc mừng Quý khách quay trúng&nbsp;<span style="font-weight: 700;">1$</span>.</p><p style="margin-bottom: 0px;">Vui lòng vào mục&nbsp;<span style="font-weight: 700;">Giỏ quà</span>&nbsp;để xem các phần thưởng.</p>',
         },
       ],
+      animation: {
+        type: 'spinToStop',
+        duration: 5,
+        spins: 8,
+        callbackFinished: this.alertPrize.bind(this),
+      },
     });
   }
 
   ngOnInit(): void {}
+
+  alertPrize(): void {
+    this.winningSegment = this.theWheel.getIndicatedSegment().text;
+    alert('You have won ' + this.theWheel.getIndicatedSegment().text);
+  }
+
+  startSpin(): void {
+    if (this.wheelSpinning === false) {
+      if (this.wheelPower === 1) {
+        this.theWheel.animation.spins = 3;
+      } else if (this.wheelPower === 2) {
+        this.theWheel.animation.spins = 8;
+      } else if (this.wheelPower === 3) {
+        this.theWheel.animation.spins = 15;
+      }
+    }
+    this.theWheel.startAnimation();
+    this.wheelSpinning = true;
+  }
 }
