a3f8934640ae5ca87a3cc85943a77e8b19ca2a53
Hai Vu Ngoc (BTS-DL) <hai.vungoc@vib.com.vn>
2021-12-30 14:43:11 +0700

    update css

--- src/app/components/win-wheel/win-wheel.component.ts
+++ src/app/components/win-wheel/win-wheel.component.ts
@@ -14,12 +14,23 @@ export class WinWheelComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit(): void {
     this.theWheel = new Winwheel({
-      outerRadius: 146, // Use these three properties to
-      centerX: 200, // correctly position the wheel
-      centerY: 201, // over the background.
+      outerRadius: 172, // Use these three properties to
+      innerRadius: 27.5, // Set inner radius to make wheel hollow.
+      centerX: 201.5, // correctly position the wheel
+      centerY: 207, // over the background.
       lineWidth: 2,
-      numSegments: 4,
+      numSegments: 12,
+      textFontSize: 14,
+      responsive: true, // This wheel is responsive!
       segments: [
+        { fillStyle: '#eae56f', text: 'Prize One' },
+        { fillStyle: '#89f26e', text: 'Prize Two' },
+        { fillStyle: '#7de6ef', text: 'Prize Three' },
+        { fillStyle: '#e7706f', text: 'Prize Four' },
+        { fillStyle: '#eae56f', text: 'Prize One' },
+        { fillStyle: '#89f26e', text: 'Prize Two' },
+        { fillStyle: '#7de6ef', text: 'Prize Three' },
+        { fillStyle: '#e7706f', text: 'Prize Four' },
         { fillStyle: '#eae56f', text: 'Prize One' },
         { fillStyle: '#89f26e', text: 'Prize Two' },
         { fillStyle: '#7de6ef', text: 'Prize Three' },
