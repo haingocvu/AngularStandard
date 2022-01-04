7c4fd69fe7e4cd4a39e8b3b122971b17e2155914
Hai Vu Ngoc (BTS-DL) <hai.vungoc@vib.com.vn>
2022-01-04 15:40:37 +0700

    complete footer component

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { MatDialog } from '@angular/material/dialog';
+import { Observable } from 'rxjs';
 
 import { IWinWheel } from '@app/interfaces/win-wheel.interface';
 import { winWheelDataSelector } from '@app/store/selectors/win-wheel.selector';
 import { IStoreState } from '@app/interfaces/store.interface';
 import { IGenericReducerState } from '@app/interfaces/general-reducer-state.interface';
-import { Observable } from 'rxjs';
 
 import { SignInComponent } from '@app/components/sign-in/sign-in.component';
+import { customerInfoDataSelector } from '@app/store/selectors/customerInfo.selector';
+import { ICustomerInfo } from '@app/interfaces/customerInfo.interface';
 
 @Component({
   selector: 'app-footer',
@@ -18,20 +20,43 @@ import { SignInComponent } from '@app/components/sign-in/sign-in.component';
 export class FooterComponent implements OnInit {
   winWheelData$: Observable<IGenericReducerState<IWinWheel>>;
   winWheelRawData: IGenericReducerState<IWinWheel> | null = null;
+  customerInfo$: Observable<IGenericReducerState<ICustomerInfo>>;
+  customerInfoRawData: IGenericReducerState<ICustomerInfo> | null = null;
 
   constructor(private store: Store<IStoreState>, public dialog: MatDialog) {
     this.winWheelData$ = this.store.select(winWheelDataSelector);
     this.winWheelData$.subscribe((data) => {
       this.winWheelRawData = data;
     });
+
+    this.customerInfo$ = this.store.select(customerInfoDataSelector);
+    this.customerInfo$.subscribe((data) => {
+      this.customerInfoRawData = data;
+    });
   }
 
   openDialogSignIn() {
-    const dialogRef = this.dialog.open(SignInComponent);
+    const isLoading = this.customerInfoRawData?.isLoading;
+    const rawData = this.customerInfoRawData?.data;
 
-    dialogRef.afterClosed().subscribe((result) => {
-      console.log(`success signin`);
-    });
+    if (!isLoading && rawData) {
+      // spin the wheel
+      console.log('spin now');
+    } else {
+      // authentication
+      const dialogRef = this.dialog.open(SignInComponent);
+      dialogRef.afterClosed().subscribe(() => {
+        console.log(`success signin`);
+      });
+    }
+  }
+
+  get isShowRemainingTurns() {
+    return this.customerInfoRawData?.data?.remainingTurns;
+  }
+
+  get getRemainingTurns() {
+    return this.customerInfoRawData?.data?.remainingTurns;
   }
 
   ngOnInit(): void {}
