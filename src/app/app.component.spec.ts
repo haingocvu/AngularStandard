397bd0a7b854b23c76b6de8a48ed17f47b011e58
Hai Vu Ngoc (BTS-DL) <hai.vungoc@vib.com.vn>
2022-01-05 19:36:36 +0700

    update name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,16 +16,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angularStandard'`, () => {
+  it(`should have as title 'luckyDraw'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angularStandard');
+    expect(app.title).toEqual('luckyDraw');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angularStandard app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'luckyDraw app is running!'
+    );
   });
 });
